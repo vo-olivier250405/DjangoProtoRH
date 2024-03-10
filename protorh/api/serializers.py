4fec74633a27eda36a72625734b8f5072eae2c92
vo_o <vo_o@etna-alternance.net>
2024-03-10 17:54:58 +0100

    [ADD => adding serializers

--- protorh/api/serializers.py
+++ protorh/api/serializers.py
@@ -10,3 +10,25 @@ class UserSerializer(serializers.ModelSerializer):
     """
     firstname = serializers.CharField(max_length=1000, required=True)
     email = serializers.CharField(max_length=1000, required=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "firstname", "email")
+
+    def create(self, validated_data):
+        """_summary_
+
+        Args:
+            validated_data (_type_): _description_
+        """
+        return User.objects.create(
+            firstname=validated_data.get("firstname"),
+            email=validated_data.get("email")
+        )
+
+    def update(self, instance, validated_data):
+        instance.firstname = validated_data.get(
+            "firstname", instance.firstname)
+        instance.email = validated_data.get("email", instance.email)
+        instance.save()
+        return instance
